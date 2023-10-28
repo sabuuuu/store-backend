6828e7d229df8657340656c32f2e614e6989f250
Yafa Sabrina <yafasabrina2323@gmail.com>
2023-10-28 21:03:57 +0100

    ready to deploy backend

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://admin:admin123@cluster0.veyhtl3.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const JWT_SECRET='3yighafond00'
